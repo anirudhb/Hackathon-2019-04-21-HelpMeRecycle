7c547319bf5cbf7b0b1ebdca94a354a3c59a37e8
tejasvissa813 <tejas.vissa@gmail.com>
2019-04-20 17:13:43 -0700

    Added Button

--- app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MainActivity.java
+++ app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ravindervissapragada.helpmerecycle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Spinner garbage = (Spinner) findViewById(R.id.trash);
-        String[] items = {"plastic","battery","paper","can","bottle"};
+        String[] items = {"plastic","battery","paper","can","bottle","other"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         garbage.setAdapter(adapter);
+        Button next = (Button) findViewById(R.id.enter);
     }
 }
