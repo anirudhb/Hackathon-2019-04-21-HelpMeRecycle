fc2f5c3343e9a574695617b3d872f2a476aa978b
tejasvissa813 <tejas.vissa@gmail.com>
2019-04-20 17:43:12 -0700

    Added Connections

--- app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MainActivity.java
+++ app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ravindervissapragada.helpmerecycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -7,15 +8,24 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner garbage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner garbage = (Spinner) findViewById(R.id.trash);
+        garbage = (Spinner) findViewById(R.id.trash);
         String[] items = {"plastic","battery","paper","can","bottle","other"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         garbage.setAdapter(adapter);
         Button next = (Button) findViewById(R.id.enter);
+
+    }
+    protected void change(){
+        String value = garbage.getSelectedItem().toString();
+        if(value != "other")
+        {
+            Intent map = new Intent(this,MapsActivity.class);
+            startActivity(map);
+        }
     }
 }
