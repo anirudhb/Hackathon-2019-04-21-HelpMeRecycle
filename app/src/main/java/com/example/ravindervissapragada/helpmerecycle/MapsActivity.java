8b4759163b83892287e6d42f56861acce8b7486a
Anirudh Balaji <anirudh.qazxswced@gmail.com>
2019-04-20 19:28:55 -0700

    Fix errors

--- app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MapsActivity.java
+++ app/src/main/java/com/example/ravindervissapragada/helpmerecycle/MapsActivity.java
@@ -21,13 +21,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
 
     private final LocationListener mLocationListener = new LocationListener() {
-        public final MapsActivity activity = this;
+        @Override
+        public void onProviderEnabled(String s) {}
+        @Override
+        public void onStatusChanged(String s, int i, Bundle b) {}
         @Override
         public void onLocationChanged(final Location location) {
             double lat = location.getLatitude();
-            double lon = location.getLongitude();
+            double lon = location.getLongitude();/*
             LatLng myLoc = new LatLng(lat, lon);
-            setMyLocation(myLoc);
+            setMyLocation(myLoc);/*
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
@@ -45,10 +48,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions()
                     .title("You Are Here")
                     .snippet("")
-                    .position(myLoc));
+                    .position(myLoc));*/
         }
         @Override
-        protected void onProviderDisable(String s) {}
+        public void onProviderDisabled(String s) {}
     };
 
     @Override
