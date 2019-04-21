954ca136bdd810311b81a9f2e8cfa92480eb59ff
Anirudh Balaji <anirudhqazxswced+github@gmail.com>
2019-04-21 03:32:33 -0700

    ADd new view.

---
+++ app/src/main/java/com/example/ravindervissapragada/helpmerecycle/CameraActivity.java
@@ -0,0 +1,15 @@
+package com.example.ravindervissapragada.helpmerecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        // Find the first camera and use it.
+    }
+}
